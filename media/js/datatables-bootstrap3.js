36619ad198b5ec7b0f32c5e9f62767327f60791c
Erik Pearson <eapearson@lbl.gov>
2017-04-15 21:20:38 -0700

    formatting fixes

--- media/js/datatables-bootstrap3.js
+++ media/js/datatables-bootstrap3.js
@@ -1,124 +1,118 @@
-
 /**
  * DataTables integration for Bootstrap 3. This requires Bootstrap 3 and
  * DataTables 1.10 or newer.
  */
 (function (factory) {
-    if ( typeof define === 'function' && define.amd ) {
+    if (typeof define === 'function' && define.amd) {
         // Define as an AMD module if possible
-        define( ['jquery', 'datatables'], factory );
-    }
-    else if ( typeof exports === 'object' ) {
+        define(['jquery', 'datatables'], factory);
+    } else if (typeof exports === 'object') {
         // Node/CommonJS
-        module.exports = factory( require('jquery'), require('datatables') );
-    }
-    else if ( jQuery ) {
+        module.exports = factory(require('jquery'), require('datatables'));
+    } else if (jQuery) {
         // Otherwise simply initialise as normal, stopping multiple evaluation
-        factory( jQuery, jQuery.fn.dataTable );
+        factory(jQuery, jQuery.fn.dataTable);
     }
 }(function ($, DataTable) {
-    "use strict";
+    'use strict';
 
 
     /* Set the defaults for DataTables initialisation */
-    $.extend( true, DataTable.defaults, {
-        dom:
-        "<'row'<'col-sm-6'l><'col-sm-6'f>>" +
-        "<'row'<'col-sm-12'tr>>" +
-        "<'row'<'col-sm-6'i><'col-sm-6'p>>",
+    $.extend(true, DataTable.defaults, {
+        dom: '<\'row\'<\'col-sm-6\'l><\'col-sm-6\'f>>' +
+            '<\'row\'<\'col-sm-12\'tr>>' +
+            '<\'row\'<\'col-sm-6\'i><\'col-sm-6\'p>>',
         renderer: 'bootstrap'
-    } );
+    });
 
 
     /* Default class modification */
-    $.extend( DataTable.ext.classes, {
-        sWrapper:      "dataTables_wrapper form-inline dt-bootstrap",
-        sFilterInput:  "form-control input-sm",
-        sLengthSelect: "form-control input-sm"
-    } );
+    $.extend(DataTable.ext.classes, {
+        sWrapper: 'dataTables_wrapper form-inline dt-bootstrap',
+        sFilterInput: 'form-control input-sm',
+        sLengthSelect: 'form-control input-sm'
+    });
 
 
     /* Bootstrap paging button renderer */
-    DataTable.ext.renderer.pageButton.bootstrap = function ( settings, host, idx, buttons, page, pages ) {
-        var api     = new DataTable.Api( settings );
+    DataTable.ext.renderer.pageButton.bootstrap = function (settings, host, idx, buttons, page, pages) {
+        var api = new DataTable.Api(settings);
         var classes = settings.oClasses;
-        var lang    = settings.oLanguage.oPaginate;
+        var lang = settings.oLanguage.oPaginate;
         var btnDisplay, btnClass;
 
-        var attach = function( container, buttons ) {
+        var attach = function (container, buttons) {
             var i, ien, node, button;
-            var clickHandler = function ( e ) {
+            var clickHandler = function (e) {
                 e.preventDefault();
-                if ( !$(e.currentTarget).hasClass('disabled') ) {
-                    api.page( e.data.action ).draw( false );
+                if (!$(e.currentTarget).hasClass('disabled')) {
+                    api.page(e.data.action).draw(false);
                 }
             };
 
-            for ( i=0, ien=buttons.length ; i<ien ; i++ ) {
+            for (i = 0, ien = buttons.length; i < ien; i++) {
                 button = buttons[i];
 
-                if ( $.isArray( button ) ) {
-                    attach( container, button );
-                }
-                else {
+                if ($.isArray(button)) {
+                    attach(container, button);
+                } else {
                     btnDisplay = '';
                     btnClass = '';
 
-                    switch ( button ) {
-                        case 'ellipsis':
-                            btnDisplay = '&hellip;';
-                            btnClass = 'disabled';
-                            break;
+                    switch (button) {
+                    case 'ellipsis':
+                        btnDisplay = '&hellip;';
+                        btnClass = 'disabled';
+                        break;
 
-                        case 'first':
-                            btnDisplay = lang.sFirst;
-                            btnClass = button + (page > 0 ?
-                                '' : ' disabled');
-                            break;
+                    case 'first':
+                        btnDisplay = lang.sFirst;
+                        btnClass = button + (page > 0 ?
+                            '' : ' disabled');
+                        break;
 
-                        case 'previous':
-                            btnDisplay = lang.sPrevious;
-                            btnClass = button + (page > 0 ?
-                                '' : ' disabled');
-                            break;
+                    case 'previous':
+                        btnDisplay = lang.sPrevious;
+                        btnClass = button + (page > 0 ?
+                            '' : ' disabled');
+                        break;
 
-                        case 'next':
-                            btnDisplay = lang.sNext;
-                            btnClass = button + (page < pages-1 ?
-                                '' : ' disabled');
-                            break;
+                    case 'next':
+                        btnDisplay = lang.sNext;
+                        btnClass = button + (page < pages - 1 ?
+                            '' : ' disabled');
+                        break;
 
-                        case 'last':
-                            btnDisplay = lang.sLast;
-                            btnClass = button + (page < pages-1 ?
-                                '' : ' disabled');
-                            break;
+                    case 'last':
+                        btnDisplay = lang.sLast;
+                        btnClass = button + (page < pages - 1 ?
+                            '' : ' disabled');
+                        break;
 
-                        default:
-                            btnDisplay = button + 1;
-                            btnClass = page === button ?
-                                'active' : '';
-                            break;
+                    default:
+                        btnDisplay = button + 1;
+                        btnClass = page === button ?
+                            'active' : '';
+                        break;
                     }
 
-                    if ( btnDisplay ) {
+                    if (btnDisplay) {
                         node = $('<li>', {
-                            'class': classes.sPageButton+' '+btnClass,
-                            'aria-controls': settings.sTableId,
-                            'tabindex': settings.iTabIndex,
-                            'id': idx === 0 && typeof button === 'string' ?
-                            settings.sTableId +'_'+ button :
-                                null
-                        } )
-                            .append( $('<a>', {
-                                'href': '#'
-                            } )
-                                .html( btnDisplay )
-                        )
-                            .appendTo( container );
+                                'class': classes.sPageButton + ' ' + btnClass,
+                                'aria-controls': settings.sTableId,
+                                'tabindex': settings.iTabIndex,
+                                'id': idx === 0 && typeof button === 'string' ?
+                                    settings.sTableId + '_' + button : null
+                            })
+                            .append($('<a>', {
+                                    'href': '#'
+                                })
+                                .html(btnDisplay)
+                            )
+                            .appendTo(container);
 
                         settings.oApi._fnBindAction(
-                            node, {action: button}, clickHandler
+                            node, { action: button }, clickHandler
                         );
                     }
                 }
@@ -136,36 +130,36 @@
      * TableTools Bootstrap compatibility
      * Required TableTools 2.1+
      */
-    if ( DataTable.TableTools ) {
+    if (DataTable.TableTools) {
         // Set the classes that TableTools uses to something suitable for Bootstrap
-        $.extend( true, DataTable.TableTools.classes, {
-            "container": "DTTT btn-group",
-            "buttons": {
-                "normal": "btn btn-default",
-                "disabled": "disabled"
+        $.extend(true, DataTable.TableTools.classes, {
+            'container': 'DTTT btn-group',
+            'buttons': {
+                'normal': 'btn btn-default',
+                'disabled': 'disabled'
             },
-            "collection": {
-                "container": "DTTT_dropdown dropdown-menu",
-                "buttons": {
-                    "normal": "",
-                    "disabled": "disabled"
+            'collection': {
+                'container': 'DTTT_dropdown dropdown-menu',
+                'buttons': {
+                    'normal': '',
+                    'disabled': 'disabled'
                 }
             },
-            "print": {
-                "info": "DTTT_print_info"
+            'print': {
+                'info': 'DTTT_print_info'
             },
-            "select": {
-                "row": "active"
+            'select': {
+                'row': 'active'
             }
-        } );
+        });
 
         // Have the collection use a bootstrap compatible drop down
-        $.extend( true, DataTable.TableTools.DEFAULTS.oTags, {
-            "collection": {
-                "container": "ul",
-                "button": "li",
-                "liner": "a"
+        $.extend(true, DataTable.TableTools.DEFAULTS.oTags, {
+            'collection': {
+                'container': 'ul',
+                'button': 'li',
+                'liner': 'a'
             }
-        } );
+        });
     }
 }));
